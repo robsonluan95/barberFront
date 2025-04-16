5021ba6650071e1d739fbcc8071ed7257d44d78a
Luan <rluan95@hotmail.com>
2025-04-15 22:39:49 -0300

    atualizacao perfil

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,30 +1,45 @@
-import {useContext} from 'react'
+import { useContext, useState } from 'react'
 import Head from 'next/head'
 import {
     Flex,
     Text,
     Heading,
-    Box ,
+    Box,
     Input,
     Link,
     Button,
 } from '@chakra-ui/react'
-import {Sidebar} from '../../componentes/siderbar'
-import {canSSRAuth} from '../../utils/canSSRAuth'
+import { Sidebar } from '../../componentes/siderbar'
+import { canSSRAuth } from '../../utils/canSSRAuth'
 import { AuthContext } from '@/src/context/AuthContext'
+import {setupAPIClient} from '../../services/api'
+
+interface UserProps {
+    id: string;
+    name: string;
+    email: string;
+    endereco: string|null;
+}
+
+interface profilePros{
+    user:UserProps
+    premium:boolean;
+}
+
+export default function Profile({user,premium}: profilePros) {
+    const { logoutUser } = useContext(AuthContext)
+    const [name,setName] = useState(user && user?.name)
+    const [endereco,setEndereco]=useState(user && user?.endereco)
 
 
-export default function Profile(){
-    const {logoutUser} = useContext(AuthContext)
-
-    async function handleLogout(){
+    async function handleLogout() {
         await logoutUser()
     }
 
-    return(
+    return (
         <>
             <Head>
-                <title>Minha Conta - BarberPRO</title>
+                <title>Minha Conta - BarberPRO</ title>
             </Head>
             <Sidebar>
                 <Flex direction="column" alignItems="flex-start" justifyContent='flex-start'>
@@ -34,70 +49,75 @@ export default function Profile(){
 
                     <Flex pt={8} pb={8} background='barber.400' maxW="700px" w="100%" direction="column" alignItems="center" justifyContent="center" >
 
-                            <Flex w="85%" direction="column" >
-                                <Text mb={2} fontSize="xl" fontWeight="bold" color="white" >Nome da barbearia</Text>
-                                <Input
-                                    w="100%"
-                                    background="gray.900"
-                                    placeholder="Nome da sua barbearia"
-                                    size="lg"
-                                    type="text"
-                                    mb={3}
+                        <Flex w="85%" direction="column" >
+                            <Text mb={2} fontSize="xl" fontWeight="bold" color="white" >Nome da barbearia</Text>
+                            <Input
+                                w="100%"
+                                background="gray.900"
+                                placeholder="Nome da sua barbearia"
+                                size="lg"
+                                type="text"
+                                mb={3}
+                                value={name}
+                                onChange={(e)=>setName(e.target.value)}
 
-                                />
+                            />
 
-                                <Text mb={2} fontSize="xl" fontWeight="bold" color="white" >Nome da barbearia</Text>
-                                <Input
-                                    w="100%"
-                                    background="gray.900"
-                                    placeholder="Endereço:"
-                                    size="lg"
-                                    type="text"
-                                    mb={3}
-                                />
-                                <Text mb={2} fontSize="xl" fontWeight="bold" color="white" >Plano atual</Text>
-                                <Flex 
-                                    direction='row'
-                                    w="100%"
-                                    mb={3}
-                                    p={1}
-                                    borderWidth={1}
-                                    rounded={6}
-                                    background="barber.900"
-                                    alignItems="center"
-                                    justifyContent="space-between"
-                                >
-                                    <Text p={2} fontSize="lg" color="#4dffb4">Gratis</Text>
-                                    <Link href="/planos" >
-                                        <Box cursor="pointer" p={1} pl={2} pr={2} background="#00cd52" rounded={4} color="white">
-                                            Mudar plano
-                                        </Box>
-                                    </Link>
-                                </Flex>
-                                <Button
-                                    w="100%"
-                                    mt={3}
-                                    mb={4}
-                                    bg="button.cta"
-                                    size="lg"
-                                    _hover={{bg:'#ffb13e'}}
-                                >
-                                    Salvar
-                                </Button>
+                            <Text mb={2} fontSize="xl" fontWeight="bold" color="white" >Nome da barbearia</Text>
+                            <Input
+                                w="100%"
+                                background="gray.900"
+                                placeholder="Endereço:"
+                                size="lg"
+                                type="text"
+                                mb={3}
+                                value={endereco}
+                                onChange={(e)=>setEndereco(e.target.value)}
 
-                                <Button 
-                                    w="100%"
-                                    mb={6}
-                                    bg="transparent"
-                                    borderWidth={2}
-                                    borderColor="red.500"
-                                    color='red.500'
-                                    _hover={{bg:'#transparent'}}
-                                    onClick={handleLogout}
-                                >
-                                    Sair
-                                </Button>
+                            />
+                            <Text mb={2} fontSize="xl" fontWeight="bold" color="white" >Plano atual</Text>
+                            <Flex
+                                direction='row'
+                                w="100%"
+                                mb={3}
+                                p={1}
+                                borderWidth={1}
+                                rounded={6}
+                                background="barber.900"
+                                alignItems="center"
+                                justifyContent="space-between"
+                            >
+                                <Text p={2} fontSize="lg" color={premium?"#FBA931":"#4dffb4"}>{premium?"Premium":"Gratis"}</Text>
+                                <Link href="/planos" >
+                                    <Box cursor="pointer" p={1} pl={2} pr={2} background="#00cd52" rounded={4} color="white">
+                                        Mudar plano
+                                    </Box>
+                                </Link>
                             </Flex>
+                            <Button
+                                w="100%"
+                                mt={3}
+                                mb={4}
+                                bg="button.cta"
+                                size="lg"
+                                _hover={{ bg: '#ffb13e' }}
+                            >
+                                Salvar
+                            </Button>
+
+                            <Button
+                                w="100%"
+                                mb={6}
+                                bg="transparent"
+                                borderWidth={2}
+                                borderColor="red.500"
+                                color='red.500'
+                                _hover={{ bg: '#transparent' }}
+                                onClick={handleLogout}
+                            >
+                                Sair
+                            </Button>
+                        </Flex>
 
 
                     </Flex>
@@ -109,10 +129,33 @@ export default function Profile(){
     )
 }
 
-export const getServerSideProps = canSSRAuth( async(ctx)=>{
-    return{
-        props:{
+export const getServerSideProps = canSSRAuth(async (ctx) => {
+    try{
+        const apiClient = setupAPIClient(ctx)
+        const response = await apiClient.get('/me')
+        
+        const user = {
+            id:response.data.id,
+            name:response.data.name,
+            email:response.data.email,
+            endereco:response.data.endereco,
+        }
+        return {
+            props:{
+                user:user,
+                premium:response.data?.subscriptions?.status === 'active' ? true : false,
+            }
+        }
 
+    }catch(err){
+        console.log(err)
+
+        return{
+            redirect:{
+                destination:'/',
+                permanent:false
+            }
         }
     }
+
 })
